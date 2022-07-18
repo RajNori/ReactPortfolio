2fca84b0cd6fe9b3ef8e4706d839280ae02bddce
Raj Nori <rajnoritech@yahoo.com>
2022-07-18 13:40:37 +1000

    navbar basic styling added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,17 @@
 // eslint-disable-next-line no-unused-expressions
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/Navbar.css'
 
 function Navbar() {
   return (
-    <div className='Navbar'>
+    <div className='navbar'>
       <div className='toggleButton'>
         <button></button>
       </div>
       <div className='links'>
         <Link to='/'> Home </Link>
-        <Link to='/projets'> Projects </Link>
+        <Link to='/projects'> Projects </Link>
         <Link to='/experience'> Experience </Link>
       </div>
     </div>
