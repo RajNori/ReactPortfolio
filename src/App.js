c164aec45a70ba1ba3ab165cfa21e6bb6c74828e
Raj Nori <rajnoritech@yahoo.com>
2022-07-18 12:55:53 +1000

    Nav added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Projects from './pages/projects';
 import Experience from './pages/Experience';
+//importing navbar as it is part of every single page the user navigates to//
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,20 @@
 // eslint-disable-next-line no-unused-expressions
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <div className="Navbar">Navbar</div>
-  )
+    <div className='Navbar'>
+      <div className='toggleButton'>
+        <button></button>
+      </div>
+      <div className='links'>
+        <Link to='/'> Home </Link>
+        <Link to='/projets'> Projects </Link>
+        <Link to='/experience'> Experience </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
