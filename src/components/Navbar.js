f6c3fcf2de032ee6a6b2185492fab9d0dc9038cf
Raj Nori <rajnoritech@yahoo.com>
2022-07-18 12:48:18 +1000

    Basic project structure outline

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line no-unused-expressions
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div className="Navbar">Navbar</div>
+  )
+}
+
+export default Navbar
